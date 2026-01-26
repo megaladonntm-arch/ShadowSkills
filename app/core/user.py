88f25c38552d87988867b60962abdc5b57a49865
megaladoNNtm <megaladonntm@gmail.com>
2026-01-26 17:48:54 +0500

    telegram bot is partly ready

---
+++ app/core/user.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+from app.data_base import base as models
+from app.schemas import user as schemas
+from app.core.security import get_password_hash
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(
+        username=user.username,
+        email=user.email,
+        full_name=user.full_name,
+        hashed_password=hashed_password,
+        is_active=True
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
