bb21ff7faee0357199f28aae768bb13112a2d213
megaladoNNtm <megaladonntm@gmail.com>
2026-01-29 20:01:19 +0500

    tried to mark mist

--- app_telegram/bot/main.py
+++ app_telegram/bot/main.py
@@ -1,29 +1,67 @@
-#main 
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart,Command
+from aiogram.types import Message
 import sys
 from pathlib import Path
-
-# Add the project root to the Python path
 project_root = Path(__file__).resolve().parents[3]
 sys.path.append(str(project_root))
-
-from handlers.start import dp, command_start_handler, command_help_handler, analyze_code
-
-from loader import bot, dp, storage
-from states import CodeAnalysis
-
-from aiogram import html
-from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
-from aiogram.filters import CommandStart, Command
+from services.ai_service import get_code_analysis
 
 
-dp.message.register(command_start_handler, CommandStart())
-dp.message.register(command_help_handler, Command("help"))
-dp.message.register(analyze_code, CodeAnalysis.waiting_for_code)
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "7994656906:AAF8RwxbZj6_iMv0KfQN-UwdTgBgin15Hog"
 
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+@dp.message(Command("analyze"))
+async def analyze_code(message: Message) -> None:
+    await message.answer("Send me a Python code snippet and I will analyze it for you.")
+
+@dp.message()
+async def analyze_code(message: Message) -> None:
+    ai_resl = get_code_analysis(message.text)
+    await message.answer(ai_resl)
+
+@dp.message()
 async def main() -> None:
-    await dp.start_polling(bot, storage=storage)
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
-    main()
-
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
